c27ae9f05dff7a5c5abb1e3c8f2674e71a2f41a9
Francis Cho <cho@pfdincome.com>
2024-12-24 10:39:02 -0800

    first commit

--- app/request/[id]/DetailComponents/ParentDetailComponents.js
+++ app/request/[id]/DetailComponents/ParentDetailComponents.js
@@ -2,12 +2,41 @@ import React from "react";
 import DetailMapComponent from "./DetailMapComponent";
 import { supabase } from "@/app/lib/supabase";
 
+//Get siggle request
 async function getSingleRequest(id) {
   try {
-    const { data, error } = await supabase
-      .from("requests")
-      .select()
-      .eq("id", id);
+    const { data, error } = await supabase.rpc("get_request_by_id", {
+      request_id: id,
+    });
+
+    console.log("get_request_by_id:: ", data);
+    if (error) throw new Error(error.message);
+    else return data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+//Get nearby drivers
+async function getNearbyDrivers(id, radius) {
+  try {
+    const { data, error } = await supabase.rpc("get_nearby_drivers", {
+      request_id: id,
+      radius_meters: radius,
+    });
+
+    if (error) throw new Error(error.message);
+    else return data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+//Get drivers
+async function getDrivers() {
+  try {
+    const { data, error } = await supabase.rpc("fetch_drivers");
+
     if (error) throw new Error(error.message);
     else return data;
   } catch (error) {
@@ -17,6 +46,15 @@ async function getSingleRequest(id) {
 
 export default async function ParentDetailComponents(props) {
   const singleRequestData = await getSingleRequest(props.requestId);
-  console.log(singleRequestData);
-  return <DetailMapComponent singleRequestData={singleRequestData} />;
+  //const driversData = await getDrivers();
+  //const nearbyDrivers = await getNearbyDrivers(props.requestId, 10000);
+  //console.log(nearbyDrivers);
+  //console.log(nearbyDrivers.length);
+  return (
+    <DetailMapComponent
+      singleRequestData={singleRequestData}
+      requestId={props.requestId}
+      //nearbyDrivers={nearbyDrivers}
+    />
+  );
 }
